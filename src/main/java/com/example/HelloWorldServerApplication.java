524e1c052364b3f6ca75e0407ccb8c6db36589dd
Victor Wibisono <vwibisono@dius.com.au>
2016-08-29 15:32:09 +1000

    Added files

---
+++ src/main/java/com/example/HelloWorldServerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldServerApplication.class, args);
+	}
+}
